efc923e13e2bee94bf420e61490376b1c496757d
lixinjian <lixinjian@nii.ac.jp>
2014-05-27 18:20:41 +0900

    Add init

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# numpy, pandas import
+
+import numpy as np
+import pandas as pd
+
+# import learning dataset
+
+from lyspy.learn.dataset import DataSet
+from lyspy.learn.test import Test
+from lyspy.learn.validate import Validate
+
+
+# import several important models
+
+from lyspy.regression.linear_regression import LinearRegression
+from lyspy.regression.logistic_regression import LogisticRegression
+from lyspy.nonlinear.neuralnetwork import NeuralNetwork
+
+
